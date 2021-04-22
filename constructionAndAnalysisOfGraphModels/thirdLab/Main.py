a3b9e3e9bc92ba3d8eb21feb988de9470ec230bb
Andrew Popov <popov_ey@mail.ru>
2021-04-22 23:02:05 +0300

    lab 3 done

--- constructionAndAnalysisOfGraphModels/thirdLab/Main.py
+++ constructionAndAnalysisOfGraphModels/thirdLab/Main.py
@@ -13,40 +13,51 @@ import matplotlib.pyplot as plt
 # список смежности графа имеет вид - (вес ребра, вершина 1, вершина 2)
 # Возраст вершин хранится в словаре ключ - номер вершины, значение - возраст
 
-# quantity = [5, 10, 20]
-quantity = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300]
+# quantity = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+quantity = [20]
 ages = {}
+global x
 x = []
 acc = []
 
-def generate_list_smezh(quantity):
+print(random.random())
+
+
+def generate_list_smezh(quantity, K):
 	global ages
 	ages = {}
 	lst_smz = []
 	for i in range(quantity):
+		ages[i] = 0
 		if len(lst_smz) < 1:
-			lst_smz += [(1, i, i)]
+			lst_smz += [(1, i, False)]
 			ages = add_ages(ages)
-			ages[i] = 0
+		# print(lst_smz)
 		else:
+			lst_smz += [(1, i, make_probability_for_real(ages, K))]
 			ages = add_ages(ages)
-			ages[i] = 0
-			lst_smz += [(1, i, make_probability_for_real(ages))]
 	# print('Размер списка смежности: ', getsizeof(lst_smz))
 	# print(lst_smz)
 	return lst_smz
 
 
-def make_probability_for_real(ages):
+def make_probability_for_real(ages, K):
 	probability_list = []
-	for i in range(len(ages)):
+	for i in range(len(ages) - 1):
 		add = ages.get(i)
-		j = 0
-		while j < add:
-			probability_list.append(i)
-			j = j + 1
-	item = random.choice(probability_list)
-	return item
+		sum_values = 0
+		for i in range(len(ages) - 1):
+			sum_values += 1 / ages.get(i)
+		A = 1 / (1 / sum_values ** K)
+		probability_list.append(A * (1 / add))
+	probability_list.sort()
+	# print(probability_list)
+	item_prob = random.random()
+	counter = 0
+	for i in probability_list:
+		if item_prob < i:
+			return ages.get(counter + 1)
+		counter += 1
 
 
 def add_ages(ages):
@@ -59,6 +70,7 @@ def plot_graf(lst_smz, name):
 	G = igraph.Graph(directed=True)
 	G.add_vertices(lst_smz[-1][1] + 1)
 	G.vs['label'] = [lst_smz[i][1] for i in range(len(lst_smz))]
+	lst_smz.pop(0)
 	for i in lst_smz:
 		G.add_edges([(i[1], i[2])])
 	# Веса
@@ -68,19 +80,16 @@ def plot_graf(lst_smz, name):
 	acc.append(G.diameter(directed=True, unconn=True, weights=None))
 
 
-
-for i in quantity:
-	for j in range(100):
-		lst_smz = generate_list_smezh(i)
-		plot_graf(lst_smz, "Граф")
-	print(acc)
-	x.append(statistics.mean(acc))
-	acc = []
-
-
-
-print(x)
-plt.plot(x, quantity)
-plt.xlabel('Ширина графа')
-plt.ylabel('Размер графа')
-plt.show()
+for K in range(3, 4):
+	for i in quantity:
+		for j in range(100):
+			lst_smz = generate_list_smezh(i, K)
+			plot_graf(lst_smz, "Граф")
+		# print(acc)
+		x.append(statistics.mean(acc))
+		acc = []
+	plt.plot(x, quantity)
+	plt.xlabel('Диаметр графа')
+	plt.ylabel('Размер графа')
+	plt.show()
+	x = []
