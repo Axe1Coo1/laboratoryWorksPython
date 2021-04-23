c77ea37c16d771ebb3396a4e9bfddeabd76d978b
Andrew Popov <popov_ey@mail.ru>
2021-04-23 18:34:55 +0300

    lab 3 done

--- constructionAndAnalysisOfGraphModels/thirdLab/Main.py
+++ constructionAndAnalysisOfGraphModels/thirdLab/Main.py
@@ -1,6 +1,6 @@
 import random
 import statistics
-
+import numpy as np
 import igraph
 from sys import getsizeof
 import matplotlib.pyplot as plt
@@ -13,16 +13,14 @@ import matplotlib.pyplot as plt
 # список смежности графа имеет вид - (вес ребра, вершина 1, вершина 2)
 # Возраст вершин хранится в словаре ключ - номер вершины, значение - возраст
 
-# quantity = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-quantity = [20]
-ages = {}
-global x
-x = []
-acc = []
-
-print(random.random())
+quantity = [5, 10, 20]  # Список размеров графа
+ages = {}  # Словарь для записи возрастов вершин
+x = []  # Список усредлнённых значений диаметров графа для построения графиков
+acc = []  # Список всех значений диаметров графа для построения графиков
 
 
+
+# Функция для генерации списка смежности графа
 def generate_list_smezh(quantity, K):
 	global ages
 	ages = {}
@@ -41,31 +39,33 @@ def generate_list_smezh(quantity, K):
 	return lst_smz
 
 
+# Функция для возврата вершины графа с которой будет сформирована связь
 def make_probability_for_real(ages, K):
-	probability_list = []
-	for i in range(len(ages) - 1):
-		add = ages.get(i)
+	probability_list = []  # Создание пустого списка для хранения вероятностей
+	for i in range(len(ages) - 1):  # Перебор элементов исключая последний, так-как он имеет нулевой возраст
+		add = ages.get(i)  # Получение возраста элемента i
 		sum_values = 0
-		for i in range(len(ages) - 1):
-			sum_values += 1 / ages.get(i)
-		A = 1 / (1 / sum_values ** K)
-		probability_list.append(A * (1 / add))
-	probability_list.sort()
-	# print(probability_list)
-	item_prob = random.random()
+		for i in range(len(ages) - 1):  # Получение суммы из возрастов / 1 в степени К
+			sum_values += 1 / (ages.get(i) ** K)
+		A = (1 / sum_values)  # Получение значения альфа
+		probability_list.append(A * (1 / add ** K))  # Добавление в список вероятностей вероятности выбора вершины i
+	item_prob = random.random()  # Генерация случайного числа в диапозоне от 0 до 1
+	probability_list = np.cumsum(probability_list)  # Получение кумулитивной суммы списка вероятностей
 	counter = 0
-	for i in probability_list:
+	for i in probability_list:  # Сравнение случайного значения с вероятностями в списке
 		if item_prob < i:
-			return ages.get(counter + 1)
+			return counter  # Возврат счётчика его значение соответстует номеру вершины, с которой нужно построить связь
 		counter += 1
 
 
+# Функция для добавления возраста вершинам
 def add_ages(ages):
 	for i in range(len(ages)):
 		ages[i] += 1
 	return ages
 
 
+# Функция для построения графа
 def plot_graf(lst_smz, name):
 	G = igraph.Graph(directed=True)
 	G.add_vertices(lst_smz[-1][1] + 1)
@@ -77,19 +77,19 @@ def plot_graf(lst_smz, name):
 	G.es['label'] = [i[0] for i in lst_smz]
 	layout = G.layout('kk')
 	igraph.plot(G, (name + '.png'), bbox=(800, 600), layout=layout, vertex_size=40, vertex_label_size=10)
-	acc.append(G.diameter(directed=True, unconn=True, weights=None))
+	acc.append(G.diameter(directed=True, unconn=True, weights=None))  # Добавление диаметра графа в список acc
 
 
-for K in range(3, 4):
+for K in range(4, 5):
 	for i in quantity:
-		for j in range(100):
+		for j in range(1):
 			lst_smz = generate_list_smezh(i, K)
 			plot_graf(lst_smz, "Граф")
 		# print(acc)
 		x.append(statistics.mean(acc))
 		acc = []
 	plt.plot(x, quantity)
-	plt.xlabel('Диаметр графа')
-	plt.ylabel('Размер графа')
-	plt.show()
 	x = []
+plt.xlabel('Диаметр графа')
+plt.ylabel('Размер графа')
+plt.show()
